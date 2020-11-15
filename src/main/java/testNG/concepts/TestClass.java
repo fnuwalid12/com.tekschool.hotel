944fe55aafbad07fa5f2f4513e9bf0f8260c6560
fnuwalid12 <73316302+fnuwalid12@users.noreply.github.com>
2020-11-15 18:59:49 -0500

    Update TestClass.java
    Changes in git hub

--- src/main/java/testNG/concepts/TestClass.java
+++ src/main/java/testNG/concepts/TestClass.java
@@ -8,5 +8,9 @@ public class TestClass {
 	@Test
 	public void test() {
 		System.out.println("This class will be pusshed to repo");
+		// This is comment from repo git hub account
+		// these are changes made in Git Repo
+		// To pull these changes we need to Fitch from upstreem or use git
+		// pull commend 
 	}
 }
