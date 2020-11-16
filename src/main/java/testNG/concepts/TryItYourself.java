9a3b289d5e6d33deeeae2cab5df32e8e26c83f50
fnuwalid12 <fnuwalid12@gmail.com>
2020-11-16 08:10:26 -0500

    Initialize this first and then the second step

---
+++ src/main/java/testNG/concepts/TryItYourself.java
@@ -0,0 +1,18 @@
+package testNG.concepts;
+
+import org.testng.annotations.Test;
+
+public class TryItYourself {
+
+	@Test
+	public void test() {
+		
+		System.out.println("This is something i tried it myself");
+		// first comment
+		// second comment
+		// third comment
+		// all comments 
+}
+	
+	
+}
